d330298e952c23d22d5bf8a3292c5bdb69cf608e
invigo <officialbayvanis@gmail.com>
2025-02-13 01:43:02 +0000

    Update core.js

--- client/js/core.js
+++ client/js/core.js
@@ -49,7 +49,7 @@ function UI() {
     this.getLocationsSize = function() {
         return lcsize;
     };
-    var socketip = "wss://127.0.0.1:443/socket.io/?EIO=3&transport=websocket"; // Change the ip to your server ip
+    var socketip = "https://realbxy.github.io/UI-client/"; // Change the ip to your server ip
     this.getSocketIP = function() {
         return socketip;
     };
